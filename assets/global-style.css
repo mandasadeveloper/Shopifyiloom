c0b0d6c43d2507166317fe2fd011957c7c0fb0c0
komal Mankani <komalmankani0104@gmail.com>
2024-02-09 18:36:51 +0530

    live theme upload

---
+++ assets/global-style.css
@@ -0,0 +1,423 @@
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;500;600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+@font-face {
+  font-family: 'Avenir LT Std';
+  src: url('AvenirLTStd-Book.woff2') format('woff2'),
+    url('AvenirLTStd-Book.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Avenir LT Std';
+  src: url('AvenirLTStd-Black.woff2') format('woff2'),
+    url('AvenirLTStd-Black.woff') format('woff');
+  font-weight: 900;
+  font-style: normal;
+  font-display: swap;
+}
+
+/**********************************************
+    - Reset
+    - Base
+**********************************************/
+:root  {
+  --font-primary: 'Montserrat','Noto Sans TC', sans-serif;
+  --font-secondary: 'Montserrat','Noto Sans TC', sans-serif;
+  --font-tertiary: 'Avenir LT Std', sans-serif;
+  --font-weight-normal: normal;
+  --font-weight-100: 100;
+  --font-weight-200: 200;
+  --font-weight-300: 300;
+  --font-weight-400: 400;
+  --font-weight-500: 500;
+  --font-weight-600: 600;
+  --font-weight-700: 700;
+  --font-weight-900: 900;
+  --font-style-normal: normal;
+  --font-style-italic: italic;
+  --line-height-normal: normal;
+  --letter-spacing-normal: normal;
+  --margin-auto: 0 auto;
+  --text-uppercase: uppercase;
+  --text-lowercase: lowercase;
+  --text-capitalize: capitalize;
+  --font-size-8: 8px;
+  --font-size-10: 10px;
+  --font-size-12: 12px;
+  --font-size-13: 13px;
+  --font-size-14: 14px;
+  --font-size-16: 16px;
+  --font-size-18: 18px;
+  --font-size-20: 20px;
+  --font-size-22: 22px;
+  --font-size-24: 24px;
+  --font-size-26: 26px;
+  --font-size-28: 28px;
+  --font-size-30: 30px;
+  --font-size-36: 36px;
+  --font-size-40: 40px;
+  --font-size-42: 42px;
+  --font-size-48: 48px;
+  --font-size-50: 50px;
+  --text-left: left;
+  --text-center: center;
+  --text-right: right;
+
+  /* Color Declaration */
+  --color-black: #000000;
+  --color-white: #ffffff;
+  --color-primary: #3C3C3C;
+  --color-grey-01: #F7F7F7;
+  --color-grey-02: #E7E5E4;
+  --color-grey-03: #F0F0F0;
+  --color-grey-04: #D6D6D6;
+  --color-grey-05: #BEBEBE;
+  --color-grey-06: #666666;
+  --color-grey-07: #2E2E2E;
+  --color-grey-08: #9A9A9A;
+  --color-grey-09: #F9F8F7;
+  --color-grey-10: #E6E6E6;
+  --color-grey-11: #F6E7D9;
+  --color-grey-12: #f1f1f1;
+  --color-grey-13: #888888;
+  --color-grey-14: #555555;
+  --color-ivory: #F3EFEA;
+  --color-pale: #BEB7B1;
+  --color-peach: #FF7A6C;
+  --color-red: #C80A1E;
+  --color-light-beige: #F2EFEA;
+  --color-transparent: transparent;
+}
+
+/* normalize css */
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+html {
+  margin: 0;
+  padding: 0;
+  scroll-behavior: smooth;
+}
+
+body {
+  margin: 0;
+  min-height: 100vh;
+  text-rendering: optimizeSpeed;
+  -webkit-font-smoothing: antialiased;
+  font-family: var(--font-primary);
+  font-style: var(--font-style-normal);
+  font-weight: var(--font-weight-normal);
+  color: var(--color-black);
+  letter-spacing: 0;
+}
+
+/* Remove all animations, transitions and smooth scroll for people that prefer not to see them */
+@media (prefers-reduced-motion: reduce) {
+  html:focus-within {
+    scroll-behavior: auto;
+  }
+}
+
+ul[role='list'],
+ol[role='list'] {
+  list-style: none;
+}
+
+a:not([class]) {
+  text-decoration-skip-ink: auto;
+}
+
+a:not([href]) {
+  cursor: not-allowed;
+}
+
+img,
+picture,
+video,
+canvas,
+svg {
+  display: block;
+  max-width: 100%;
+}
+
+input,
+button,
+textarea,
+select {
+  font: inherit;
+  color: var(--color-black);
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  overflow-wrap: break-word;
+  color: var(--color-black);
+}
+
+div,
+span,
+applet,
+object,
+iframe,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+del,
+dfn,
+em,
+font,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend {
+  outline: 0;
+  border: 0;
+  padding: 0;
+  margin: 0;
+  vertical-align: baseline;
+  font: inherit;
+  color: var(--color-black);
+}
+
+a {
+  text-decoration: none;
+  color: var(--color-black);
+}
+
+/* utility classes*/
+.content-left {
+  text-align: var(--text-left);
+}
+
+.content-center {
+  text-align: var(--text-center);
+}
+
+.content-right {
+  text-align: var(--text-right);
+}
+
+.align-center {
+  align-items: center;
+  justify-content: center;
+}
+
+.overflow-hidden {
+  overflow: hidden;
+}
+
+.hide {
+  display: none;
+}
+
+.visibility-hidden {
+  visibility: hidden;
+}
+
+.break-word {
+  word-break: break-word;
+}
+
+.text-ellipsis {
+  white-space: nowrap;
+  word-wrap: normal;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.text-p {
+  font-family: var(--font-family);
+  font-style: var(--font-style-normal);
+  font-weight: var(--font-weight-normal);
+  font-size: var(--font-size-content);
+}
+
+.text-p--primaryFont {
+  --font-family: var(--font-primary);
+}
+
+.text-p--secondaryFont {
+  --font-family: var(--font-secondary);
+}
+
+.text-p--8 {
+  --font-size-content: var(--font-size-8);
+}
+
+.text-p--10 {
+  --font-size-content: var(--font-size-10);
+}
+
+.text-p--12 {
+  --font-size-content: var(--font-size-12);
+}
+
+.text-p--13 {
+  --font-size-content: var(--font-size-13);
+}
+
+.text-p--14 {
+  --font-size-content: var(--font-size-14);
+}
+
+.text-p--16 {
+  --font-size-content: var(--font-size-16);
+}
+
+.text-p--18 {
+  --font-size-content: var(--font-size-18);
+}
+
+.text-p--20 {
+  --font-size-content: var(--font-size-20);
+}
+
+.text-p--24 {
+  --font-size-content: var(--font-size-24);
+}
+
+.text-p--26 {
+  --font-size-content: var(--font-size-26);
+}
+
+.text-p--28 {
+  --font-size-content: var(--font-size-28);
+}
+
+.text-p--30 {
+  --font-size-content: var(--font-size-30);
+}
+
+.text-p--36 {
+  --font-size-content: var(--font-size-36);
+}
+
+.text-p--40 {
+  --font-size-content: var(--font-size-40);
+}
+
+.text-p--42 {
+  --font-size-content: var(--font-size-42);
+}
+
+.text-p--48 {
+  --font-size-content: var(--font-size-48);
+}
+
+.text-p--50 {
+  --font-size-content: var(--font-size-50);
+}
+
+.uppercase {
+  text-transform: var(--text-uppercase);
+}
+
+.capitalize {
+  text-transform: var(--text-capitalize);
+}
+
+.lowercase {
+  text-transform: var(--text-lowercase);
+}
+
+html,
+body {
+  word-break: keep-all;
+}
+
+.button-white,
+.button-black {
+  background: var(--color-transparent);
+  font-size: var(--font-size-14);
+  padding: 12px;
+  outline: none;
+  font-family: var(--font-secondary);
+  font-weight: var(--font-weight-normal);
+}
+
+.button-white {
+  border: 1px solid var(--color-white);
+  color: var(--color-white);
+}
+
+.button-black {
+  border: 1px solid var(--color-grey);
+  color: var(--color-grey);
+}
+
+.button-white:hover,
+.button-black:hover {
+  background: var(--color-grey);
+  color: var(--color-white);
+}
+
+.il-backToTop {
+  right: 40px;
+  width: 48px;
+  height: 48px;
+  bottom: 40px;
+  z-index: 999;
+  display: none;
+  position: fixed;
+  border-radius: 100%;
+  align-items: center;
+  justify-content: center;
+  color: var(--color-primary);
+  background-color: var(--color-grey-01);
+  box-shadow: 0px 0px 10px 0px #0000001F;
+}
+
+.il-backToTop .icon {
+  color: var(--color-primary);
+}
